7175f389497bbedee227f186dfa0db54262ef18b
andToxa <andToxa@gmail.com>
2020-12-26 19:03:28 +0700

    Luhn Algorithm

---
+++ CodeBattle.Hexlet.IO/LuhnAlgorithmTests.cs
@@ -0,0 +1,48 @@
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+using static System.Int32;
+
+namespace CodeBattle.Hexlet.IO
+{
+    public class LuhnAlgorithmTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public LuhnAlgorithmTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Theory]
+        [InlineData("49927398716", true)]
+        [InlineData("49927398717", false)]
+        [InlineData("1234567812345670", true)]
+        [InlineData("1234567812345678", false)]
+        [InlineData("79927398710", false)]
+        [InlineData("79927398711", false)]
+        [InlineData("79927398712", false)]
+        [InlineData("79927398713", true)]
+        [InlineData("79927398714", false)]
+        [InlineData("79927398715", false)]
+        [InlineData("79927398716", false)]
+        [InlineData("79927398717", false)]
+        [InlineData("79927398718", false)]
+        [InlineData("79927398719", false)]
+        [InlineData("374652346956782346957823694857692364857368475368", true)]
+        [InlineData("374652346956782346957823694857692364857387456834", false)]
+        [InlineData("8", false)]
+        [InlineData("0", true)]
+        public void LuhnAlgorithmTest(string cardNumber, bool isValidExpected)
+        {
+            var cardNumberReverseArray = cardNumber.Reverse().ToArray();
+            var sum = cardNumberReverseArray
+                .Select((t, i) => i % 2 == 0
+                    ? Parse(t.ToString())
+                    : Parse(t.ToString()) * 2 / 10 + Parse(t.ToString()) * 2 % 10)
+                .Sum();
+            var isValid = sum % 10 == 0;
+            Assert.Equal(isValidExpected, isValid);
+        }
+    }
+}
