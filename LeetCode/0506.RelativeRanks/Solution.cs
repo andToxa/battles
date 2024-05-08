41b4d56e310545a624d4e087e483353c868b649c
andToxa <andToxa@gmail.com>
2024-05-08 13:31:02 +0300

    LeetCode 506. Relative Ranks

---
+++ LeetCode/0506.RelativeRanks/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode._0506.RelativeRanks;
+
+public class Solution
+{
+    public string[] FindRelativeRanks(int[] score)
+    {
+        var answer = new string[score.Length];
+
+        for (var i = 1; i <= score.Length; i++)
+        {
+            var maxValue = score.Max();
+            var maxIndex = Array.IndexOf(score, maxValue);
+            score[maxIndex] = -1;
+            answer[maxIndex] = i switch
+            {
+                1 => "Gold Medal",
+                2 => "Silver Medal",
+                3 => "Bronze Medal",
+                _ => i.ToString()
+            };
+        }
+        
+        return answer;
+    }
+}
---
+++ LeetCode/0506.RelativeRanks/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode._0506.RelativeRanks;
+
+public class SolutionTests
+{
+    public static TheoryData<int[], string[]> TestData => new()
+    {
+        { [5, 4, 3, 2, 1], ["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"] },
+        { [10, 3, 8, 9, 4], ["Gold Medal", "5", "Bronze Medal", "Silver Medal", "4"] },
+        { [1], ["Gold Medal"] },
+        { [], [] },
+    };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int[] score, string[] expectedOutput)
+    {
+        var solution = new Solution();
+        var output = solution.FindRelativeRanks(score);
+
+        output.Should().Equal(expectedOutput);
+    }
+}
