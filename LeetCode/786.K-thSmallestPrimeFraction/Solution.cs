7a285d39f6f7acd83c76ab7362241fc719ae914f
andToxa <andToxa@gmail.com>
2024-05-10 18:22:12 +0300

    LeetCode 786. K-th Smallest Prime Fraction

---
+++ LeetCode/786.K-thSmallestPrimeFraction/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._786.K_thSmallestPrimeFraction;
+
+public class Solution
+{
+    public int[] KthSmallestPrimeFraction(int[] arr, int k)
+    {
+        var fractions = new SortedDictionary<double, (int, int)>();
+        for (var i = 0; i < arr.Length - 1; i++)
+        {
+            for (var j = i + 1; j < arr.Length; j++)
+            {
+                fractions.Add((double)arr[i] / arr[j], (arr[i], arr[j]));
+            }
+        }
+
+        return new[] { fractions.ElementAt(k - 1).Value.Item1, fractions.ElementAt(k - 1).Value.Item2 };
+    }
+}
